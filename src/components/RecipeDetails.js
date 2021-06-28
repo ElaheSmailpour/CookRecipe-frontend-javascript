0a00698b66331f58e02427aa581e3d31050e21b8
Elahesmailpour <eli_es87@yahoo.com>
2021-06-28 17:24:58 +0200

    cookrezpt

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -1,21 +1,15 @@
-import { v4 as uuidv4 } from 'uuid'
-const RecipeDetails = ({ ingredients }) => {
-    return (
-        <div className="RecipeDetails">
-            {ingredients.map(ingredient => {
-                return (
-                    <ul key={uuidv4()} className="ingredient-list">
-                        <li className="ingredient-text">
-                            {ingredient.text}
-                        </li>
-                        <li className="ingredient-weight">weight-
-    {ingredient.weight}
-                        </li>
-                    </ul>
-                )
-            })}
-        </div>
-    )
-}
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
 
-export default RecipeDetails
+const RecipeDetails = ({ ingredients }) => {
+  return ingredients.map(ingredient => {
+    return (
+      <ul key={uuidv4()} className="ingredient-list">
+        <li className="ingredient-text">{ingredient.text}</li>
+        <li className="ingredient-weight">Weight - {ingredient.weight}</li>
+      </ul>
+    );
+  });
+};
+
+export default RecipeDetails;
